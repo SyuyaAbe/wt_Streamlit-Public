3389631c8b2a06e8c2d1854e227db16f13577279
S.A <s.a0107hokkaidou.fuh@gmail.com>
2024-11-20 23:18:06 +0900

    Update wt_app_csv.py

--- wt_app_csv.py
+++ wt_app_csv.py
@@ -10,8 +10,8 @@ st.title('ワートリ 隊員パラメータ')
 
 
 # CSVファイルを読み込み
-df = pd.read_csv('/Users/syuya/Desktop/develop/wt/params.csv')
-equipment_df = pd.read_csv('/Users/syuya/Desktop/develop/wt/equipment.csv')
+df = pd.read_csv('./params.csv')
+equipment_df = pd.read_csv('./equipment.csv')
 
 
 # 'name'列が存在しない場合、日本語列名である可能性を考慮
